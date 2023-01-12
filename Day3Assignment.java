f8209d23fb1d08657706f394d5fdae78fd2e4a2a
Ritesh Kumar <121331179+RiteshKumar1331@users.noreply.github.com>
2023-01-12 20:38:39 +0530

    Add files via upload

---
+++ Day3Assignment.java
@@ -0,0 +1,18 @@
+package com.CodeWithHarry;
+
+import java.util.Scanner;
+
+public class Day3Assignment {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int n = sc.nextInt();
+        if (n > 0)
+            System.out.println("The entered number is a positive number");
+        else if (n == 0)
+            System.out.println("The entered number is zero");
+        else
+            System.out.println("The entered number is a negative number");
+        System.out.println("Program Ended");
+    }
+}
